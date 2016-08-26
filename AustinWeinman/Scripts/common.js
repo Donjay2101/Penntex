bfa03b0637710e9f0c78a7d08b5bd14bce00c6cb
donjay2101 <deep.21jan@live.com>
2016-08-26 05:15:18 -0600

    deepak back button

--- AustinWeinman/Scripts/common.js
+++ AustinWeinman/Scripts/common.js
@@ -19,3 +19,62 @@
         });
     }
 }
+
+
+
+
+function goToPrevious(url)
+{
+    debugger;
+    //console.log(getURL());
+    var txt = decodeURI(url);
+    //url =  txt;
+    //window.location.assign("http://localhost:53676/")
+    if (url == "")
+    {
+        window.location.href = "/";
+    }
+    else
+    {
+        window.location.href = url;
+    }
+    
+    //location.href='../'+txt;
+}
+
+
+function getURL()
+{
+    var arr = window.location.href.split("/");
+    var baseURl = "http://";
+    for (i = 2; i <3; i++)
+    {
+        baseURl += arr[i];
+    }
+
+    return baseURl+"/";
+}
+
+function goToCreate(url) {
+
+    debugger;
+    var cururl = window.location.href;
+    var comidx = cururl.split('/');
+    var returnUrl="";
+    for (i = 3; i < comidx.length; i++) {
+        if (comidx[i] != "#" && comidx[i] != "") {
+
+            returnUrl += "/" + comidx[i];
+        }
+
+    }
+    var newurl = "";
+    if (url.indexOf('?') > 0) {
+        newurl = url + "&returnUrl=" + returnUrl;;
+    }
+    else {
+        newurl = url + "?returnUrl=" + returnUrl;
+    }
+
+    window.location.href = newurl;
+}
